953bf11048ea0c627205116b7f1f14df4ddd5ce0
joseanosoares <joseanosoaresbb@gmail.com>
2023-02-07 23:52:00 -0300

    .

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -2,11 +2,11 @@ const menorValor = 1;
 const maiorValor = 100;
 const numeroSecreto = gerarNumeroAleatorio();
 
-function gerarNumeroAleatorio(){
+function gerarNumeroAleatorio() {
     return parseInt(Math.random() * maiorValor + 1); //parseInt transformar um número inteiro. Math.random para gerar um número entre 0-1. *100 + 1 para esse número ser entre 0-99 + 1;
 }
 
-console.log('Número Secreto:', gerarNumeroAleatorio());
+console.log('Número Secreto:', numeroSecreto);
 
 const elementoMenorValor = document.getElementById('menor-valor'); 
 elementoMenorValor.innerHTML = menorValor; //innerHTML muda o valor dentro da classe menor-valor
